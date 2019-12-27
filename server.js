dc3687fd4025c8c8ee12f5fb2dbd8ffc3aac8d3b
jiyoung-lee <jno0717@naver.com>
2019-12-27 15:16:00 +0900

    server 소스 수정

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.use(session({
     secret: 'asadlfkj!@#',
